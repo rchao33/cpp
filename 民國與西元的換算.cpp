3332c2bbfbf6e24d6393e032878d5418f8a5b882
rchao33 <48470606+rchao33@users.noreply.github.com>
2019-03-17 21:20:51 +0800

    Create 民國與西元的換算.cpp

---
+++ 民國與西元的換算.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std;  
+  
+int main()  
+{  
+    int n;  //宣告 
+  
+    cin >> n;  //輸入 
+    cout << n-1911 << endl;//讓西元減1911為輸出  
+  
+    return 0;  
+}
