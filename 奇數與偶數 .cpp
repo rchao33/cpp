cef1151575acb59cb118f3742643daa0695de562
rchao33 <48470606+rchao33@users.noreply.github.com>
2019-03-17 21:25:26 +0800

    Create 奇數與偶數 .cpp

---
+++ 奇數與偶數 .cpp
@@ -0,0 +1,16 @@
+#include  <iostream>      
+      
+using namespace std;      
+      
+int main()      
+{         
+    int n; //宣告   
+    
+cin >> n;//輸入 
+if(n%2==0)  //如果n除以2的餘數是0 
+cout<<"EVEN"<<endl;  //就輸出EVEN 
+else  //否則 
+cout<<"ODD"<<endl;//就輸出OOD  
+    
+return 0;    
+}
