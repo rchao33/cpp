e937aa2688dff19bb74c98d1b3135e501cc2fabb
rchao33 <48470606+rchao33@users.noreply.github.com>
2019-03-17 21:23:02 +0800

    Create 矩形的面積 .cpp

---
+++ 矩形的面積 .cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std;  
+  
+int main()  
+{  
+    int a,b; //宣告有兩個數字 
+  
+    cin >> a>>b;//輸入  
+    cout << a*b << endl;//兩個數字相乘輸出  
+  
+    return 0;  
+}
